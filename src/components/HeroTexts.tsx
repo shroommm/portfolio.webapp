884e2f481594a7cec94743c82e6a698694b76e84
shroommm <khacnguyenquan@gmail.com>
2024-07-09 11:52:47 +0700

    feat: minor font weight changes at home page

--- src/components/HeroTexts.tsx
+++ src/components/HeroTexts.tsx
@@ -3,7 +3,7 @@ import TextRotator from "./TextRotator";
 const HeroTexts = () => {
   return (
     <>
-      <h3 className="font-poppins text-2xl max-sm:text-xl">My Name is</h3>
+      <h3 className="text-2xl max-sm:text-xl font-semibold">Hi, my name is</h3>
       <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl ">
         Nguyen Khac Anh Quan
       </h1>
--- src/components/TextRotator.tsx
+++ src/components/TextRotator.tsx
@@ -6,8 +6,8 @@ function TextRotator() {
     <div
     className="py-4 rounded-md flex flex-col justify-center overflow-hidden"
   >
-    <div className="font-poppins text-base sm:text-2xl [text-wrap:balance] text-gray-700">
-    I am a Web Developer &
+    <div className="text-base sm:text-2xl font-medium [text-wrap:balance] text-gray-700">
+    I am a Web Developer & a
       <span
         className="inline-flex ml-2 flex-col h-[calc(theme(fontSize.lg)*theme(lineHeight.tight))] sm:h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] overflow-hidden"
       >
